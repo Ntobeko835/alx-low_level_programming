ef9d45820647fe486beaad9a5ac781303b5244eb
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-06-07 13:18:45 -0700

    now

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_INCLUDED
+
 #define MAIN_H_INCLUDED
-{
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,6 +15,7 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+
 #endif
-}
+
 return (0);
