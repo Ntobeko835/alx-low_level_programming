2d8f674d8206e9d9100ac79b36c57b461928d63f
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-06-07 07:11:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/cvorrect)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar('\n');
+		return (0);
+	}
+}
