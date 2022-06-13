0922f53703309eb43a4be6701853770d7ab316bf
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-06-13 09:50:24 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _puts - it puts to stdout
+ * _puts - puts stdout
  * @str: parameter
  */
-
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0');
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
+
 	_putchar('\n');
 }
