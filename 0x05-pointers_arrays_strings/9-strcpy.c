7dd4459d48947d1e826ec4173c2062e7d573363f
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-06-13 14:24:32 -0700

    copy strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,9 @@ char *_strcpy(char *dest, char *srs)
 {
 	int i;
 
-	for (i = 0; *(src + i) != '\0'; i++)
+	for (i = 0; *(srs + i) != '\0'; i++)
 	{
-		dest[i] = *(src + i);
+		dest[i] = *(srs + i);
 	}
 	dest[i] = '\0';
 	return (dest);
