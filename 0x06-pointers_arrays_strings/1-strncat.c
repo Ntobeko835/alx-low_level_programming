68f1a8fda9b4531f69748b4d0257572a4cd32a27
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-06-18 22:17:05 -0700

    task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,20 +6,28 @@
  * @n: value
  * Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
+
 {
 	int i, j;
+
 	i = 0;
 	while (dest[i] != '\0')
+
 	{
 		i++;
 	}
+
 	j = 0;
 	while (j < n && src[j] != '\0')
+
 	{
-		i++;
 		j++;
+		i++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
