71062912e86bcd8d6ff25938b15afeb756b202b2
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-06-21 09:17:10 -0700

    advanced 2

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,7 +18,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (*(n2 + c2) != '\0')
 		c2++;
 	if (c1 >= c2)
-	bg = c1;
+		bg = c1;
 	else
 		bg = c2;
 	if (size_r <= bg + 1)
@@ -26,7 +26,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n1 + c2) - 48;
-	while (bg > 0)
+	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
 		if (op >= 10)
