d55ac1c68d75d8f6f49ab139879d2f0285f0359e
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-07-01 17:10:50 -0700

    advanced

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,13 +15,14 @@ int count_word(char *s)
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
-		flag = 0;
+			flag = 0;
 		else if (flag == 0)
 		{
 			flag = 1;
 			w++;
 		}
 	}
+
 	return (w);
 }
 /**
@@ -35,16 +36,15 @@ char **strtow(char *str)
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
-		while (*(str + len))
-			len++;
-			words = count_word(str);
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
 
-			if (words == 0)
-
-				return (NULL);
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
-	return (NULL);
+		return (NULL);
 
 	for (i = 0; i <= len; i++)
 	{
@@ -53,22 +53,20 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-			tmp = (char *) malloc(sizeof(char) * (c + 1));
-		if (tmp == NULL)
-		return (NULL);
-
-		while (start < end)
-			*tmp++ = str[start++];
-
-		*tmp = '\0';
-		matrix[k] = tmp - c;
-		k++;
-		c = 0;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
 		}
 		else if (c++ == 0)
-		start = i;
-		}
-		matrix[k] = NULL;
-		return (matrix);
+			start = i;
+	}
+	matrix[k] = NULL;
+	return (matrix);
 }
