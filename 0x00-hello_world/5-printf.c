4854fd5fbeba749700ec71428ec5dd14421a21a3
Ntobeko835 <ntobekozamokuhle835@gmail.com>
2022-08-18 23:32:14 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - beg 
- * 
- * Return: stop
+ * main - entry point
+ *
+ * Return: ending
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
